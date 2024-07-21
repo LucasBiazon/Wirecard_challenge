f59d2a0dc7cde0270f64bf0945e7ecab73ee104e
Lucas <lucasbiazonpalma@gmail.com>
2024-07-21 18:39:39 -0300

    Feat: func main

--- cmd/wirecard.go
+++ cmd/wirecard.go
@@ -9,6 +9,15 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/lucasBiazon/wirecard/internal/handler"
+	"github.com/phenpessoa/gutils/netutils/httputils"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func main() {
@@ -16,17 +25,60 @@ func main() {
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGKILL)
 	defer cancel()
 
+	if err := godotenv.Load(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading .env file: %v\n", err)
+		os.Exit(1)
+	}
+
 	if err := run(ctx); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
-	fmt.Println("Wirecard service stopped")
+	fmt.Println("goodbye :)")
 }
 
 func run(ctx context.Context) error {
+	cfg := zap.NewDevelopmentConfig()
+	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	logger, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+
+	logger = logger.Named("wirecard_app")
+	defer func() { _ = logger.Sync() }()
+
+	dbUser := os.Getenv("WIRECARD_POSTGRES_USER")
+	dbPassword := os.Getenv("WIRECARD_POSTGRES_PASSWORD")
+	dbHost := os.Getenv("WIRECARD_POSTGRES_HOST")
+	dbPort := os.Getenv("WIRECARD_POSTGRES_PORT")
+	dbName := os.Getenv("WIRECARD_POSTGRES_DB")
+
+	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" {
+		return fmt.Errorf("one or more required environment variables are not set")
+	}
+
+	connString := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		dbUser, dbPassword, dbHost, dbPort, dbName,
+	)
+
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		return err
+	}
+	r := chi.NewMux()
+	r.Use(middleware.RequestID, middleware.Recoverer, httputils.ChiLogger(logger))
+
+	r.Post("/payments", handler.CreatePaymentHandler)
 
 	srv := &http.Server{
 		Addr:         ":8080",
+		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
@@ -38,9 +90,10 @@ func run(ctx context.Context) error {
 		defer cancel()
 
 		if err := srv.Shutdown(ctx); err != nil {
-			fmt.Print("failed to shutdown server")
+			logger.Error("failed to shutdown server", zap.Error(err))
 		}
 	}()
+
 	errChan := make(chan error, 1)
 
 	go func() {
